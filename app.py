fe2e94856ff1edd7a6bce8b9a3252015dcc9e0c6
moshe filosof <funart@funart.co.il>
2025-02-14 18:39:29 +0200

    Updated app.py - removed debug=True

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 # עמוד הבית - בדיקה שהשרת עובד
 @app.route("/")
 def home():
-    return "Flask פועל בהצלחה ב-Railway!"
+    return "Flask פועל בהצלחה ב-Docker!"
 
 # דף נוסף לבדיקה
 @app.route("/test")
 def test():
-    return "בדיקת Route נוסף ב-Railway!"
+    return "בדיקת Route נוסף ב-Docker!"
 
 # הרצת האפליקציה עם ההגדרות הנכונות
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)  # הסרת debug=True
